ad52687c5cd446820dd7dba518495aeed203831c
So-Emily <esoriano110@gmail.com>
2024-08-05 20:29:22 -0700

    added logout code

---
+++ public/js/logout.js
@@ -0,0 +1,13 @@
+// public/js/logout.js
+document.querySelector('#logout').addEventListener('click', async () => {
+    const response = await fetch('/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/login');
+    } else {
+        alert('Failed to log out.');
+    }
+});
