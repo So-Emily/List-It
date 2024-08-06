d9ad1e2b7c4c2967b55a3f602f4d8da17234deac
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-08-06 14:30:49 -0700

    updated api list routes for the CRUD operations

--- controllers/api/listRoutes.js
+++ controllers/api/listRoutes.js
@@ -1,43 +1,45 @@
 const router = require('express').Router();
 const { List } = require('../../models');
 
-
 // POST a new list
-router.post('/', async (req, res) => {
-    try {
-        const newList = await List.create(req.body);
-        res.status(200).json(newList);
-    } catch (err) {
-        res.status(400).json(err);
-    }
+router.post('/create', async (req, res) => {
+  try {
+    const newList = await List.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newList);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // PUT to update a list
-router.put('/:id', async (req, res) => {
-    try {
-        const updatedList = await List.update(req.body, {
-        where: {
-            id: req.params.id,
-        },
-        });
-        res.status(200).json(updatedList);
-    } catch (err) {
-        res.status(400).json(err);
-    }
+router.put('/update/:id', async (req, res) => {
+  try {
+    const updatedList = await List.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(updatedList);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // DELETE a list
-router.delete('/:id', async (req, res) => {
-    try {
-        const listData = await List.destroy({
-        where: {
-            id: req.params.id,
-        },
-        });
-        res.status(200).json(listData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+router.delete('/delete/:id', async (req, res) => {
+  try {
+    const listData = await List.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(listData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
