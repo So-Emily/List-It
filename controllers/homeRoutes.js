40ee65d7a5262d2544ffd53e132fd54736ebc6bd
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-08-08 20:24:36 -0700

    created route to lists page and included all lists for that categ ory

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,31 +1,47 @@
 const router = require('express').Router();
+const { where } = require('sequelize');
 const checkAuthentication = require('../middlewares/middlewares');
 
+const { User, List } = require('../models');
+
 router.get('/', async (req, res) => {
-    
-    try {
-        res.render('login', {logged_in:req.session.logged_in})
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+    res.render('login', { logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // Handler for the homepage
 router.get('/homepage', checkAuthentication, async (req, res) => {
-    try {
-        res.render('homepage', {logged_in:req.session.logged_in });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+    res.render('homepage', { logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // Handler for the register
 router.get('/register', async (req, res) => {
-    try {
-        res.render('register', {logged_in:req.session.logged_in});
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+    res.render('register', { logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('view/list/:category', async (req, res) => {
+  try {
+
+    req.session.category = req.params.category;
+    const allLists = await List.getAll({ where: {
+        categorys:req.params.category
+    } });
+    const list = allLists.map((list) => list.get({ plain: true }));
+    res.render('lists', {list, logged_in: req.session.logged_in  });
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
 });
 
 module.exports = router;
